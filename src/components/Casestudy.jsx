8e935bfa08d391476d34d81b46f17f82a3529a87
korana <korayut@gmail.com>
2023-04-26 22:47:37 +0700

    feat: add link download PDF from google drive

--- src/components/Casestudy.jsx
+++ src/components/Casestudy.jsx
@@ -14,6 +14,15 @@ const Casestudy = () => {
             <p className="text-xl font-bold my-2">
               1. Karentoroku App: Available Time Booking
             </p>
+            <p className="text-yellow-500">
+              If PDF viewer is not available, please click{" "}
+              <a
+                href="https://drive.google.com/file/d/1Q4eeHmz8msx8EmdglVB5E6rf4S7s6fXm/view?usp=sharing"
+                className="text-blue-600"
+              >
+                here
+              </a>
+            </p>
             <iframe
               src="https://drive.google.com/file/d/1Q4eeHmz8msx8EmdglVB5E6rf4S7s6fXm/preview"
               width="640"
@@ -25,6 +34,15 @@ const Casestudy = () => {
             <p className="text-xl font-bold my-2">
               2. KS Movie Reservation App
             </p>
+            <p className="text-yellow-500">
+              If PDF viewer is not available, please click{" "}
+              <a
+                href="https://drive.google.com/file/d/1Y2L4alYaE_sHfUmz7MXcj-ctSab9Dpsm/view?usp=sharing"
+                className="text-blue-600"
+              >
+                here
+              </a>
+            </p>
             <iframe
               src="https://drive.google.com/file/d/1Y2L4alYaE_sHfUmz7MXcj-ctSab9Dpsm/preview"
               width="640"
@@ -35,6 +53,15 @@ const Casestudy = () => {
           <div>
             <p className="text-xl font-bold my-2">
               3. Karentoroku Website: Available Time Booking
+            </p>
+            <p className="text-yellow-500">
+              If PDF viewer is not available, please click{" "}
+              <a
+                href="https://drive.google.com/file/d/1bUjiIhcE8PZyzjJx862M_bdY3ixhN_w0/view?usp=sharing"
+                className="text-blue-600"
+              >
+                here
+              </a>
             </p>
             <iframe
               src="https://drive.google.com/file/d/1bUjiIhcE8PZyzjJx862M_bdY3ixhN_w0/preview"
